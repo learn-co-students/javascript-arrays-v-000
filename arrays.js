f2d4313a888d5dbcf19371cef961157cc87abe69
Daniel Suarez <suarez.daniel101@gmail.com>
2017-01-02 04:13:26 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -2,7 +2,24 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
 var myarray = [1];
-
 mynewarray = ["foo", ...myarray];
 return mynewarray;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+var myarray = [1];
+myarray = ["foo", ...myarray];
+return myarray;
+}
+
+function addElementToEndOfArray(array, element) {
+var anotherarray = [1];
+anewarray = [...anotherarray, "foo"];
+return anewarray;
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+var anotherarray = [1];
+anotherarray = [...anotherarray, "foo"];
+return anotherarray;
 }
