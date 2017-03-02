edb9fa9fb52fd3c20175d251971af7cc0c62ff1c
Lena Sawyer <lenasawyer93@gmail.com>
2017-03-02 17:36:42 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,5 +1,29 @@
 var chocolateBars = ['snickers','hundred grand','kitkat','skittles'];
+
 function addElementToBeginningOfArray (array, element)
-  {return [element,...array]}
+  {return [element, ...array]
+}
 function destructivelyAddElementToBeginningOfArray (array,element)
-  {return array.unshift (element);}
+  {return array.unshift (element), array;
+  }
+function addElementToEndOfArray (array,element){
+  return [...array,element];
+}
+function destructivelyAddElementToEndOfArray(array,element){
+  return array.push (element), array;
+  }
+function accessElementInArray (array,index){
+  return array[2]
+}
+function destructivelyRemoveElementFromBeginningOfArray (array){
+  return array.shift (array[1])
+}
+function removeElementFromBeginningOfArray (array){
+  return array.slice(1)
+}
+function destructivelyRemoveElementFromEndOfArray (array) {
+  return array.pop ()
+}
+function removeElementFromEndOfArray (array) {
+  return array.slice (0,array.length -1)
+}
