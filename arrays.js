1816d70fd6a43acfa194c1b8d1d931bfa0a63f79
Holger Paas <holger.paas@gmail.com>
2017-04-10 19:45:39 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,13 +1,11 @@
 var chocolateBars = ['snickers','hundred grand','kitkat','skittles'];
 
 function addElementToEndOfArray(array, element){
-  array.push(element)
-  return array
+  return [...array,element]
 }
 
 function addElementToBeginningOfArray(array,element){
-  array.unshift(element)
-  return array  
+  return [element,...array]  
 }
 
 function removeElementFromBeginningOfArray(array){
