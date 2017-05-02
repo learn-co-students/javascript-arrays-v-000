cb22aea3bfc272a280cc88cd984d96863a2908e9
Cesar  Serrano <serrano598@gmail.com>
2017-05-02 05:26:06 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,21 @@
+// var ingredient1 = "bread"
+// var ingredient2 = "mild cheese"
+// var ingredient3 = "sharp cheese"
+// var ingredient4 = "butter"
+// var ingredient5 = "tomato"
+// var ingredient6 = "garlic"
+
+var chocolateBars =[ "snickers", "hundred grand", "kitkat","skittles"]
+
+function addElementToBeginningOfArray (array, element){
+  var myArray ['foo', 1]
+myArray = [1, ...myArray]
+return myArray
+}
+
+function destructivelyAddElementToBeginningOfArray (array,element){
+  var cities = ["New York", "San Francsico"]
+  cities.unshift ("San Diego")
+}
+// function addElementToEndOfArray (,) {}
+// function destructivelyAddElementToEndOfArray (,) {}
