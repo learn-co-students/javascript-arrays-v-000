020f5880bc02f7bc8f7355d53ae482c056fe43f8
Sergio Mendoza <smendoza787@gmail.com>
2017-06-26 17:32:20 -0700

    Done.

---
+++ arrays.js
@@ -0,0 +1,48 @@
+var chocolateBars = [
+  "snickers",
+  "hundred grand",
+  "kitkat",
+  "skittles"
+];
+
+function addElementToBeginningOfArray(arr, elem) {
+  return [elem, ...arr];
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, elem) {
+  arr.unshift(elem);
+  return arr;
+}
+
+function addElementToEndOfArray(arr, elem) {
+  return [...arr, elem];
+}
+
+function destructivelyAddElementToEndOfArray(arr, elem) {
+  arr.push(elem);
+  return arr;
+}
+
+function accessElementInArray(arr, i) {
+  return arr[i];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(arr) {
+  arr.shift();
+  return arr;
+}
+
+function removeElementFromBeginningOfArray(arr) {
+  var new_arr = arr.slice(1);
+  return new_arr;
+}
+
+function destructivelyRemoveElementFromEndOfArray(arr) {
+  arr.pop();
+  return arr;
+}
+
+function removeElementFromEndOfArray(arr) {
+  new_arr = arr.slice(0, arr.length -1);
+  return new_arr;
+}
