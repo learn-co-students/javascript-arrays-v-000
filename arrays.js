0a61fe31d640f2ffc197e1b31dc2247ed14dc380
William Hicks <w.hicks917@gmail.com>
2017-08-03 19:32:16 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,37 @@
+
+
+var chocolateBars = ['snickers','hundred grand', 'kitkat', 'skittls']
+
+function addElementToBeginningOfArray(array, element) {array.unshift(element);return array}
+
+function addElementToEndOfArray(array, element) {array.push(element); return array}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {array.unshift(element);return array}
+
+function destructivelyAddElementToEndOfArray(array, element) {array.push(element); return array}
+
+function accessElementInArray(array, index) {
+  for (let i=0;i<array.length;i++) {
+    if (i == index) {
+      return array[i];
+    }
+  }
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.splice(1)
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1);
+}
