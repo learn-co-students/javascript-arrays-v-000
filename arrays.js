5ab3f334573ba1ce0e5e581fcf3cce8d07612757
mirandalily <mirandaraymond@me.com>
2017-08-12 22:15:46 -0700

    3 tests passing

--- arrays.js
+++ arrays.js
@@ -1,1 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+var addElementToBeginningOfArray = (array, element) => {
+  newArray = [element, ...array]
+  return newArray
+};
