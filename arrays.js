1cb91a5c6be3a0834aaaf13d0e88cd180bead498
mirandalily <mirandaraymond@me.com>
2017-08-21 18:36:05 -0700

    destructively remove element from beginning of array

--- arrays.js
+++ arrays.js
@@ -21,3 +21,8 @@ function destructiveAddElementToEndOfArray(array, element){
 function accessElementInArray(array, index){
   return array[index]
 }
+
+function destructiveRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array
+}
